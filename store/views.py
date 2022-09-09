6e3ac34f790d7f510f3b1eb987a0fce4dfac5400
ambroseotundo <ambroseotundo9@gmail.com>
2022-09-09 05:57:35 -0400

    return all db products with the serializer views

--- store/views.py
+++ store/views.py
@@ -1,13 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
 from rest_framework.decorators import api_view
+from rest_framework import status
 from rest_framework.response import Response # this is for the API
+from  .models import Product
+from .serializers import ProductSerializer
+
 
 @api_view()
 def product_list(request):
-    return Response('ok')
+    queryset = Product.objects.all()
+    serializer = ProductSerializer(queryset, many=True)
+    return Response(serializer.data)
 
 
 @api_view()
 def product_detail(request, id):
-    return Response('ok')
+    product = get_object_or_404(Product, pk=id)
+    serializer = ProductSerializer(product)
+    return Response(serializer.data)
+   
+
+
 
