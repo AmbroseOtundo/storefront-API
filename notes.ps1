141c3d43a5729c89015d22db3351e5fa24f642d5
ambroseotundo <ambroseotundo9@gmail.com>
2022-09-12 12:13:22 -0400

    notes in ps1

--- notes.ps1
+++ notes.ps1
@@ -138,3 +138,85 @@ def product_list(request):
 
 # Saving data in the DB
  serializer.save()
+
+#  ADVANCED API CONCEPTS
+# Class Based Views
+from rest_framework.views import APIView
+-- this is the base class of all class based views
+
+#  Mixins
+is a class that encapsulates a code
+-- first we import from rest_framework.mixins import ListModelMixin, CreateModelMixin
+# Generic views
+-- they combine two or more mixins
+-- example : from rest_framework.generics import ListCreateAPIView
+
+
+# Customizing our generic views
+-- original code
+            @api_view(['GET', 'PUT', 'DELETE'])
+def collection_detail(request, pk):
+    collection = get_object_or_404(
+        Collection.objects.annotate(
+            products_count=Count('products')), pk=pk)
+    if request.method == 'GET':        
+        serializer = CollectionSerializer(collection)
+        return Response(serializer.data)
+    elif request.method == 'PUT':
+        serializer = CollectionSerializer(Collection, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
+    elif request.method == 'DELETE':
+        # This is checking if the product has any order items. If it does, it returns a 405 error.
+        if collection.products.count() > 0:
+            return Response({'error':'Collection cannot be deleted because it includes one or more products'},status=status.HTTP_405_METHOD_NOT_ALLOWED)
+        collection.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+    # After customization
+    class ProductList(ListCreateAPIView):
+    queryset = Product.objects.select_related('collection').all() 
+    serializer_class = ProductSerializer
+    def get_serializer_context(self):
+        return {'request':self.request}
+    
+    NOTE: If some code has some  logic e.g if else statements we leave them as they are.
+    example: The delete handler has some logic
+        class ProductDetail(RetrieveUpdateDestroyAPIView):
+        queryset = Product.objects.all()
+        serializer_class = ProductSerializer
+        def delete(self, request, id):
+            product = get_object_or_404(Product, pk=id)
+            # This is checking if the product has any order items. If it does, it returns a 405 error.
+            if product.orderitems.count() > 0:
+                return Response({'error':'Product cannot be deleted because it is associated with another product'},status=status.HTTP_405_METHOD_NOT_ALLOWED)
+            product.delete()
+            return Response(status=status.HTTP_204_NO_CONTENT)
+# Viewsetsw
+
+This is a set of related views.
+
+from rest_framework.viewsets import ViewSet
+
+class ProductViewSet(ModelViewSet):
+    queryset = Product.objects.all() 
+    serializer_class = ProductSerializer
+     
+    def get_serializer_context(self):
+        return {'request':self.request}
+    def delete(self, request, pk):
+        product = get_object_or_404(Product, pk=pk)
+        # This is checking if the product has any order items. If it does, it returns a 405 error.
+        if product.orderitems.count() > 0:
+            return Response({'error':'Product cannot be deleted because it is associated with another product'},status=status.HTTP_405_METHOD_NOT_ALLOWED)
+        product.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+# Routers
+--We use this when we employ viewsets in the url.py file
+-- from rest_framework.routers import SimpleRouter -- do this in the url file
+router = SimpleRouter()
+router.register('product', views.ProductViewSet)
+
