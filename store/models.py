308bbf4f3d2703f348e959ce38c2063c12026dad
ambroseotundo <ambroseotundo9@gmail.com>
2022-09-14 09:33:05 -0400

    add constraint in the CartItem Model

--- store/models.py
+++ store/models.py
@@ -1,4 +1,3 @@
-from telnetlib import PRAGMA_HEARTBEAT
 from django.core.validators import MinValueValidator
 from django.db import models
 from uuid import uuid4
@@ -101,9 +100,12 @@ class Cart(models.Model):
 
 
 class CartItem(models.Model):
-    cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE, related_name='items')
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     quantity = models.PositiveSmallIntegerField()
+
+    class Meta:
+        unique_together = [['cart', 'product']]
 
 
 class Review(models.Model):
