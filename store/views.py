8b99b4789afefde8ca094c8328234ce067f98fee
ambroseotundo <ambroseotundo9@gmail.com>
2022-09-20 11:03:15 -0400

    applying model permissions

--- store/views.py
+++ store/views.py
@@ -3,7 +3,6 @@ from rest_framework.decorators import api_view
 from rest_framework.mixins import CreateModelMixin, RetrieveModelMixin, DestroyModelMixin, UpdateModelMixin
 from rest_framework.viewsets import ViewSet, ModelViewSet, GenericViewSet
 from rest_framework.permissions import IsAuthenticated, AllowAny, IsAdminUser
-from rest_framework import status
 from rest_framework.decorators import action
 from rest_framework.response import Response
 from django_filters.rest_framework import DjangoFilterBackend
